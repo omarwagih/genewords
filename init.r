21da090dc29aa658f24e10bb2c5fd346d62899a4
Omar Wagih <omarwagih@gmail.com>
2015-05-17 13:56:14 +0100

    buildpack file

--- init.r
+++ init.r
@@ -1,4 +1,4 @@
-install.packages("RSQLite", dependencies = TRUE))
-install.packages("sqldf", dependencies = TRUE))
-install.packages("stringi", dependencies = TRUE))
+install.packages("RSQLite", dependencies = TRUE)
+install.packages("sqldf", dependencies = TRUE)
+install.packages("stringi", dependencies = TRUE)
 
